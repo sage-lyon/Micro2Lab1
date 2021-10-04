1c59b4bcc37aa5a1b698505b25208cbacaf34542
sage-lyon <sage_lyon@student.uml.edu>
2021-10-03 23:11:57 -0400

    Added comments and cleaned up code

--- Lab1/Lab1.ino
+++ Lab1/Lab1.ino
@@ -1,63 +1,71 @@
-const int Red_LED = 22;
-const int Yellow_LED = 24;
-const int Green_LED = 26;
-const int Buzzer = 28;
-const int Buzzer_Frequency = 1750;
+/* Microprocessor Systems II & Embedded Systems
+ *  EECE.4520 (201)
+ *  Lab 1
+ *  Sage Lyon & Matthew Sylvester
+ */
+
+const int redLED = 22;
+const int yellowLED = 24;
+const int greenLED = 26;
+const int buzzer = 28;
+const int buzzerFrequency = 1750;
 const byte interruptPin = 2;
 volatile byte state = LOW;
+int buttonPressed = 0;
 
-int Button_Pressed = 0;
 void setup() {
-  pinMode(Red_LED, OUTPUT);
-  pinMode(Yellow_LED, OUTPUT);
-  pinMode(Green_LED, OUTPUT);                                      
-
-  pinMode(Buzzer, OUTPUT);
-  
+  // Set LED and buzzer pins to output
+  pinMode(redLED, OUTPUT);
+  pinMode(yellowLED, OUTPUT);
+  pinMode(greenLED, OUTPUT);                                      
+  pinMode(buzzer, OUTPUT);
+  // Set button pin to input pullup and attach interupt to pin
   pinMode(interruptPin, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(interruptPin), start, RISING);
 
 }
 
 void loop() {
-  if(Button_Pressed == 0){
-      digitalWrite(Red_LED, HIGH);
+  // If button is not pressed then turn red LED blinks
+  if(buttonPressed == 0){
+      digitalWrite(redLED, HIGH);
       delay(1000);
-      digitalWrite(Red_LED, LOW);
+      digitalWrite(redLED, LOW);
       delay(1000);
     }
-    else{
-      digitalWrite(Red_LED, HIGH);
+  // If button is pressed then cycle through R-G-Y LEDs and beep for three seconds before light changes
+  else{
+      digitalWrite(redLED, HIGH);
       delay(17000);
       beep();
-      digitalWrite(Red_LED, LOW);
-      digitalWrite(Yellow_LED, HIGH);
+      digitalWrite(redLED, LOW);
+      digitalWrite(yellowLED, HIGH);
       beep();
-      digitalWrite(Yellow_LED, LOW);
-      digitalWrite(Green_LED, HIGH);
+      digitalWrite(yellowLED, LOW);
+      digitalWrite(greenLED, HIGH);
       delay(17000);
       beep();
-      digitalWrite(Green_LED, LOW);
-      }
+      digitalWrite(greenLED, LOW);
+  }
 }
 
-
+// Sets buttonPressed to 1 when button is pressed
 void start(){
-    Button_Pressed = 1;
-    
+    buttonPressed = 1;
   }
 
+// Buzzer beeps on and off for three seconds
 void beep(){
-  tone(Buzzer, Buzzer_Frequency);
+  tone(buzzer, buzzerFrequency);
   delay(500);
-  noTone(Buzzer);
+  noTone(buzzer);
   delay(500);
-  tone(Buzzer, Buzzer_Frequency);
+  tone(buzzer, buzzerFrequency);
   delay(500);
-  noTone(Buzzer);
+  noTone(buzzer);
   delay(500);
-  tone(Buzzer, Buzzer_Frequency);
+  tone(buzzer, buzzerFrequency);
   delay(500);
-  noTone(Buzzer);
+  noTone(buzzer);
   delay(500);
 }
